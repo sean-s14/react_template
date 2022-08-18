bbcd5d2981dfc8cf69a401706a354187963574b5
Shaun Stocker <s.stocker04@outlook.com>
2022-08-19 00:04:18 +0100

    Installed @mui/material and react-router-dom. Created Home and About pages and implemented navigation.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from './pages/homePage';
+import { AboutPage } from './pages/aboutPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="about" element={<AboutPage />}/>
+      </Routes>
     </div>
   );
 }
