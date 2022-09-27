d98a0a205cb9855b4fd370cf2a37d37a250fcb39
Shaun Stocker <s.stocker04@outlook.com>
2022-09-27 15:27:23 +0100

    Removed useless components

--- src/components/exports.js
+++ src/components/exports.js
@@ -1,11 +1,5 @@
 import Input from './Input';
-import InputSubmit from './InputSubmit';
-import FormButton from './FormButton';
-import Form from './Form';
 
 export {
     Input,
-    InputSubmit,
-    FormButton,
-    Form,
 }
